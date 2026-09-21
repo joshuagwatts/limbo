86595c6c7d955f7f2ccf659ed9953245246e245a
muse <muse@localhost>
2026-09-21 04:49:27 +0000

    Build 50: build stamp 50, cache bust (root)

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
  * successful (ok) responses).
  */
 
-const BUILD = '49';
+const BUILD = '50';
 const CACHE = `limbo-v${BUILD}`;
 
 /* Same version stamps the page uses (?v=40). query strings are part of
